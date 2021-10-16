4d1d2f44ed7afe620ee5809bd408cfd86f8c120a
Kevin N <54434994+kvndngyn@users.noreply.github.com>
2021-10-16 12:19:12 -1000

    landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Grid, Button, Container } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <Container fluid>
+        <Grid verticalAlign='middle' textAlign="center" className="landing-page">
+          <Grid.Column width={8}>
+            <h3 className="landing-header2">Manage Your Projects With</h3>
+            <h1 className="landing-header1">Nebula</h1>
+            <Link to='/signin'>
+              <Button size='huge' className="landing-page-button">Get Started</Button>
+            </Link>
+          </Grid.Column>
+        </Grid>
+      </Container>
     );
   }
 }
