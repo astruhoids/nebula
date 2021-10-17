b0c8232a208c33745769cecb6f44bd9e689b0c92
Deshay Clemons <dclemons@hawaii.edu>
2021-10-16 16:59:58 -1000

    #3 Second Attempt at Drag/Drop
    - Uses React Beautiful Dnd
    - Will continue cleaning up stuff and use our own custom data instead

--- app/imports/ui/pages/ProjectBoard.jsx
+++ app/imports/ui/pages/ProjectBoard.jsx
@@ -1,139 +1,200 @@
 import React from 'react';
-import { Card, Container, Header } from 'semantic-ui-react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { Card, CardContent, Container, Header } from 'semantic-ui-react';
+
+const getItems = (count, offset = 0) => Array.from({ length: count }, (v, k) => k).map(k => ({
+  id: `item-${k + offset}`,
+  content: `item ${k + offset}`,
+}));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  return result;
+};
 
 class ProjectBoard extends React.Component {
-  // Issue that is currently being dragged around the screen
-  dragging = (e) => {
-    // Ensuring it's a proper issue
-    if ((/issue\d+/g).test(e.target.id)) {
-      e.dataTransfer.setData('text', e.target.id);
-      e.dataTransfer.effectAllowed = 'move';
-    }
-  }
 
-  // When the issue has been dropped in one of the valid drop-fields
-  dropped(e) {
-    e.preventDefault();
-    // Retrieving the content of the issue being dropped
-    const location = e.dataTransfer.getData('text');
-    if ((/(todo|progress|done)/g).test(e.target.id)) {
-      // Appending the issue at the end of the drop-fields childrens
-      // eslint-disable-next-line no-undef
-      e.target.appendChild(document.getElementById(location));
-    }
-  }
+  state = {
+    items: getItems(10),
+    selected: getItems(5, 10),
+  };
 
-  // When an issue is dragged over a valid drop-field
-  draggedOver(e) {
-    e.preventDefault();
-    // Setting dropEffect to move so the issues is able to move
-    e.dataTransfer.dropEffect = 'move';
-  }
+  id2List = {
+    droppable: 'items',
+    droppable2: 'selected',
+  };
+
+  getList = id => this.state[this.id2List[id]];
+
+  onDragEnd = result => {
+    const { source, destination } = result;
+
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
+
+    if (source.droppableId === destination.droppableId) {
+      const items = reorder(
+        this.getList(source.droppableId),
+        source.index,
+        destination.index,
+      );
+
+      let state = { items };
+
+      if (source.droppableId === 'droppable2') {
+        state = { selected: items };
+      }
+
+      this.setState(state);
+    } else {
+      const result = move(
+        this.getList(source.droppableId),
+        this.getList(destination.droppableId),
+        source,
+        destination,
+      );
+
+      this.setState({
+        items: result.droppable,
+        selected: result.droppable2,
+      });
+    }
+  };
 
   render() {
+    const list = [
+      {
+        id: 'issue1',
+        header: 'issue1',
+        text: 'stuff',
+      },
+      {
+        id: 'issue2',
+        header: 'issue2',
+        text: 'stuff',
+      },
+      {
+        id: 'issue3',
+        header: 'issue3',
+        text: 'stuff',
+      },
+      {
+        id: 'issue4',
+        header: 'issue4',
+        text: 'stuff',
+      },
+      {
+        id: 'issue5',
+        header: 'issue5',
+        text: 'stuff',
+      },
+    ];
+
     return (
       <Container>
-        <Header as="h1" textAlign="center">Project Board</Header>
-        <Card.Group centered stackable>
-          <Card>
-            <Card.Header>To-Do</Card.Header>
-            <Card.Content
-              id='todo'
-              className='cardPanel'
-              onDragOver={(e) => this.draggedOver(e)}
-              onDrop={(e) => {
-                this.dropped(e);
-              }}
-            >
-              <Card
-                id='issue1'
-                draggable
-                onDragStart={(e) => this.dragging(e)}
-              >
-                <Card.Content>
-                  <Card.Header>Issue 1</Card.Header>
-                </Card.Content>
-                <Card.Content>
-                  Stuff
-                </Card.Content>
-              </Card>
-              <Card
-                id='issue2'
-                draggable
-                onDragStart={(e) => this.dragging(e)}
-              >
-                <Card.Content>
-                  <Card.Header>Issue 2</Card.Header>
-                </Card.Content>
-                <Card.Content>
-                  Stuff
-                </Card.Content>
-              </Card>
-              <Card
-                draggable
-                id='issue3'
-                onDragStart={(e) => this.dragging(e)}
-              >
-                <Card.Content>
-                  <Card.Header>Issue 3</Card.Header>
-                </Card.Content>
-                <Card.Content>
-                  Stuff
-                </Card.Content>
-              </Card>
-              <Card
-                draggable
-                id='issue4'
-                onDragStart={(e) => this.dragging(e)}
-              >
-                <Card.Content>
-                  <Card.Header>Issue 4</Card.Header>
-                </Card.Content>
-                <Card.Content>
-                  Stuff
-                </Card.Content>
-              </Card>
-              <Card
-                draggable
-                id='issue5'
-                onDragStart={(e) => this.dragging(e)}
-              >
-                <Card.Content>
-                  <Card.Header>Issue 5</Card.Header>
-                </Card.Content>
-                <Card.Content>
-                  Stuff
-                </Card.Content>
-              </Card>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Header>In Progress</Card.Header>
-            <Card.Content
-              id='progress'
-              className='cardPanel'
-              onDragOver={(e) => this.draggedOver(e)}
-              onDrop={(e) => {
-                this.dropped(e);
-              }}
-            >
-              {/* Content */}
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Header>Completed</Card.Header>
-            <Card.Content
-              id='done'
-              className='cardPanel'
-              onDragOver={(e) => this.draggedOver(e)}
-              onDrop={(e) => {
-                this.dropped(e);
-              }}
-            >
-              {/* Content */}
-            </Card.Content>
-          </Card>
-        </Card.Group>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Card.Group>
+            <Card>
+              <Card.Content>
+                <Card.Header>To Do</Card.Header>
+              </Card.Content>
+              <Card.Content className='cardPanel'>
+                <Droppable droppableId="droppable">
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                    >
+                      {this.state.items.map((item, index) => (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}>
+                          {(provided) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <Card>
+                                <Card.Content>
+                                  <Card.Header>{item.id}</Card.Header>
+                                </Card.Content>
+                                <Card.Content>
+                                  {item.content}
+                                </Card.Content>
+                              </Card>
+                            </div>
+                          )}
+                        </Draggable>
+
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </Card.Content>
+            </Card>
+            <Card>
+              <CardContent>
+                <Card.Header>In Progress</Card.Header>
+              </CardContent>
+              <Card.Content className='cardPanel'>
+                <Droppable droppableId="droppable2">
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                    >
+                      {this.state.selected.map((item, index) => (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}>
+                          {(provided) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <Card>
+                                <Card.Content>
+                                  <Card.Header>{item.id}</Card.Header>
+                                </Card.Content>
+                                <Card.Content>
+                                  {item.content}
+                                </Card.Content>
+                              </Card>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </Card.Content>
+            </Card>
+          </Card.Group>
+        </DragDropContext>
       </Container>
     );
   }
