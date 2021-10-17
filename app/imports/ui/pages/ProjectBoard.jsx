4da5933373db29898ca66e1818708395e2bea155
Deshay Clemons <dclemons@hawaii.edu>
2021-10-17 13:27:34 -1000

    #3 Updated ProjectBoard.jsx
    - Made it with dynamic setState variables
    - Works with three columns

--- app/imports/ui/pages/ProjectBoard.jsx
+++ app/imports/ui/pages/ProjectBoard.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { Card, CardContent, Container, Header } from 'semantic-ui-react';
 
-const getItems = (count, offset = 0) => Array.from({ length: count }, (v, k) => k).map(k => ({
-  id: `item-${k + offset}`,
-  content: `item ${k + offset}`,
-}));
-
-// a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
@@ -20,7 +14,7 @@ const move = (source, destination, droppableSource, droppableDestination) => {
   const sourceClone = Array.from(source);
   const destClone = Array.from(destination);
   const [removed] = sourceClone.splice(droppableSource.index, 1);
-
+  
   destClone.splice(droppableDestination.index, 0, removed);
 
   const result = {};
@@ -32,20 +26,69 @@ const move = (source, destination, droppableSource, droppableDestination) => {
 
 class ProjectBoard extends React.Component {
 
+  list = [
+    {
+      id: 'issue1',
+      header: 'issue1',
+      text: 'stuff',
+    },
+    {
+      id: 'issue2',
+      header: 'issue2',
+      text: 'stuff',
+    },
+    {
+      id: 'issue3',
+      header: 'issue3',
+      text: 'stuff',
+    },
+    {
+      id: 'issue4',
+      header: 'issue4',
+      text: 'stuff',
+    },
+    {
+      id: 'issue5',
+      header: 'issue5',
+      text: 'stuff',
+    },
+  ];
+
   state = {
-    items: getItems(10),
-    selected: getItems(5, 10),
+    todo: this.list,
+    progress: [
+      {
+        id: 'test1',
+        header: 'test1',
+        text: 'test1',
+      },
+      {
+        id: 'test3',
+        header: 'test3',
+        text: 'test3',
+      },
+    ],
+    done: [
+      {
+        id: 'test2',
+        header: 'test2',
+        text: 'test2',
+      },
+    ],
   };
 
-  id2List = {
-    droppable: 'items',
-    droppable2: 'selected',
+  idList = {
+    todo: 'todo',
+    progress: 'progress',
+    done: 'done',
   };
 
-  getList = id => this.state[this.id2List[id]];
+  getList = id => this.state[this.idList[id]];
 
   onDragEnd = result => {
     const { source, destination } = result;
+    console.log(source);
+    console.log(destination);
 
     // dropped outside the list
     if (!destination) {
@@ -60,13 +103,20 @@ class ProjectBoard extends React.Component {
       );
 
       let state = { items };
+      console.log(state);
 
-      if (source.droppableId === 'droppable2') {
-        state = { selected: items };
+      if (source.droppableId === 'progress') {
+        state = { progress: items };
+      } else if (source.droppableId === 'done') {
+        state = { done: items };
       }
 
       this.setState(state);
+
     } else {
+      const columnA = source.droppableId;
+      const columnB = destination.droppableId;
+
       const result = move(
         this.getList(source.droppableId),
         this.getList(destination.droppableId),
@@ -75,59 +125,33 @@ class ProjectBoard extends React.Component {
       );
 
       this.setState({
-        items: result.droppable,
-        selected: result.droppable2,
+        [columnA]: result.[columnA],
+        [columnB]: result.[columnB],
       });
     }
   };
 
   render() {
-    const list = [
-      {
-        id: 'issue1',
-        header: 'issue1',
-        text: 'stuff',
-      },
-      {
-        id: 'issue2',
-        header: 'issue2',
-        text: 'stuff',
-      },
-      {
-        id: 'issue3',
-        header: 'issue3',
-        text: 'stuff',
-      },
-      {
-        id: 'issue4',
-        header: 'issue4',
-        text: 'stuff',
-      },
-      {
-        id: 'issue5',
-        header: 'issue5',
-        text: 'stuff',
-      },
-    ];
-
     return (
       <Container>
+        <Header as='h1' textAlign='center'>Project</Header>
         <DragDropContext onDragEnd={this.onDragEnd}>
-          <Card.Group>
+          <Card.Group textAlign='center'>
             <Card>
               <Card.Content>
                 <Card.Header>To Do</Card.Header>
               </Card.Content>
               <Card.Content className='cardPanel'>
-                <Droppable droppableId="droppable">
+                <Droppable droppableId="todo">
                   {(provided) => (
                     <div
                       ref={provided.innerRef}
                     >
-                      {this.state.items.map((item, index) => (
+                      {provided.placeholder}
+                      {this.state.todo.map(({ id, header, text }, index) => (
                         <Draggable
-                          key={item.id}
-                          draggableId={item.id}
+                          key={id}
+                          draggableId={id}
                           index={index}>
                           {(provided) => (
                             <div
@@ -137,18 +161,16 @@ class ProjectBoard extends React.Component {
                             >
                               <Card>
                                 <Card.Content>
-                                  <Card.Header>{item.id}</Card.Header>
+                                  <Card.Header>{header}</Card.Header>
                                 </Card.Content>
                                 <Card.Content>
-                                  {item.content}
+                                  {text}
                                 </Card.Content>
                               </Card>
                             </div>
                           )}
                         </Draggable>
-
                       ))}
-                      {provided.placeholder}
                     </div>
                   )}
                 </Droppable>
@@ -159,15 +181,15 @@ class ProjectBoard extends React.Component {
                 <Card.Header>In Progress</Card.Header>
               </CardContent>
               <Card.Content className='cardPanel'>
-                <Droppable droppableId="droppable2">
+                <Droppable droppableId='progress'>
                   {(provided) => (
                     <div
                       ref={provided.innerRef}
                     >
-                      {this.state.selected.map((item, index) => (
+                      {this.state.progress.map(({ id, header, text }, index) => (
                         <Draggable
-                          key={item.id}
-                          draggableId={item.id}
+                          key={id}
+                          draggableId={id}
                           index={index}>
                           {(provided) => (
                             <div
@@ -177,10 +199,49 @@ class ProjectBoard extends React.Component {
                             >
                               <Card>
                                 <Card.Content>
-                                  <Card.Header>{item.id}</Card.Header>
+                                  <Card.Header>{header}</Card.Header>
                                 </Card.Content>
                                 <Card.Content>
-                                  {item.content}
+                                  {text}
+                                </Card.Content>
+                              </Card>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </Card.Content>
+            </Card>
+            <Card>
+              <CardContent>
+                <Card.Header>Done</Card.Header>
+              </CardContent>
+              <Card.Content className='cardPanel'>
+                <Droppable droppableId='done'>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                    >
+                      {this.state.done.map(({ id, header, text }, index) => (
+                        <Draggable
+                          key={id}
+                          draggableId={id}
+                          index={index}>
+                          {(provided) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <Card>
+                                <Card.Content>
+                                  <Card.Header>{header}</Card.Header>
+                                </Card.Content>
+                                <Card.Content>
+                                  {text}
                                 </Card.Content>
                               </Card>
                             </div>
