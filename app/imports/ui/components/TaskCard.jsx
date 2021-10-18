b16143b34a364800e91d5266b82c899b823a0291
Gabriel Undan <gundan@hawaii.edu>
2021-10-17 18:47:04 -1000

    #10 Added TaskCard component

---
+++ app/imports/ui/components/TaskCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+/** Is shown on the project page. Represents a task to create a part */
+class TaskCard extends React.Component {
+  render() {
+    return (
+      <Card className="taskCard">
+        <Card.Content>
+          <Card.Header>{this.props.part.name}</Card.Header>
+          <Card.Meta>Quantity: {this.props.part.quantity}</Card.Meta>
+          <Card.Description>
+            Assigned to: {this.props.part.assignee}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+TaskCard.propTypes = {
+  part: PropTypes.object.isRequired
+};
+
+export default TaskCard;
